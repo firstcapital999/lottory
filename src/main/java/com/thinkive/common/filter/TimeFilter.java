dd522b9075b83fa1710842445f7e7e9e3e3af4cc
382517195 <382517195@qq.com>
2017-10-17 09:52:09 +0800

    增加过滤器demo

---
+++ src/main/java/com/thinkive/common/filter/TimeFilter.java
@@ -0,0 +1,33 @@
+package com.thinkive.common.filter;
+
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Date;
+
+@Component
+public class TimeFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+        System.out.println("time filter init");
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("time filter start");
+        long start = new Date().getTime();
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("time filter 耗时："+(new Date().getTime()-start));
+        System.out.println("time filter finish");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("time filter destory");
+    }
+}
