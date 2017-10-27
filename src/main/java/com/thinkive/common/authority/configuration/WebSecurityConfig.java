5f92ba3f4ae50c612bb55bd994eab69e1d38f518
382517195@qq.com <382517195@qq.com>
2017-10-27 22:01:05 +0800

    提交前端抽奖的代码

--- src/main/java/com/thinkive/common/authority/configuration/WebSecurityConfig.java
+++ src/main/java/com/thinkive/common/authority/configuration/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/home","/register","/regist/**","/testShort","/yourls/*","/images/**","/css/**","/js/**","/**/*.html").permitAll()//访问：/home 无需登录认证权限
+                .antMatchers("/","/home","/register","/regist/**","/testShort","/yourls/*","/images/**","/css/**","/scripts/**","/**/*.html").permitAll()//访问：/home 无需登录认证权限
                 .anyRequest().authenticated() //其他所有资源都需要认证，登陆后访问
                 .antMatchers("/hello").hasAuthority("ADMIN") //登陆后之后拥有“ADMIN”权限才可以访问/hello方法，否则系统会出现“403”权限不足的提示
                 .and()
