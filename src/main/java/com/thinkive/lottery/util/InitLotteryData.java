4f98e523b762cc8d71956ac18d7b8e72ee5925a0
382517195@qq.com <382517195@qq.com>
2017-11-05 11:05:01 +0800

    初始化数据代码

---
+++ src/main/java/com/thinkive/lottery/util/InitLotteryData.java
@@ -0,0 +1,13 @@
+package com.thinkive.lottery.util;
+
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ *
+ */
+public class InitLotteryData implements CommandLineRunner {
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
