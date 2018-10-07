9de23e6228bbbf917e53504bc10164e43536cdc8
dcn <382517195@qq.com>
2018-10-07 23:00:05 +0800

    rediscluster密码集群

--- src/main/java/com/thinkive/common/redis/configuration/RedisFactoryConfig.java
+++ src/main/java/com/thinkive/common/redis/configuration/RedisFactoryConfig.java
@@ -1,10 +1,5 @@
 package com.thinkive.common.redis.configuration;
 
-import io.lettuce.core.RedisURI;
-import io.lettuce.core.cluster.ClusterClientOptions;
-import io.lettuce.core.cluster.RedisClusterClient;
-import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
-import io.lettuce.core.resource.ClientResources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +7,11 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-import javax.annotation.Resource;
-import java.time.Duration;
-import java.time.temporal.TemporalUnit;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description ${DESCRIPTION}
@@ -35,11 +27,13 @@ public class RedisFactoryConfig {
     @Bean
     public RedisConnectionFactory myLettuceConnectionFactory() {
         Map<String, Object> source = new HashMap<String, Object>();
+        String password = environment.getProperty("spring.redis.password");
         source.put("spring.redis.cluster.nodes", environment.getProperty("spring.redis.cluster.nodes"));
         source.put("spring.redis.cluster.timeout", environment.getProperty("spring.redis.cluster.timeout"));
         source.put("spring.redis.cluster.max-redirects", environment.getProperty("spring.redis.cluster.max-redirects"));
         RedisClusterConfiguration redisClusterConfiguration;
         redisClusterConfiguration = new RedisClusterConfiguration(new MapPropertySource("RedisClusterConfiguration", source));
+        redisClusterConfiguration.setPassword(RedisPassword.of(password));
         return new LettuceConnectionFactory(redisClusterConfiguration);
     }
 
